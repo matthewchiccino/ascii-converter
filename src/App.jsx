09590d6841f4d909bc6ae4b25d4d40d8f4350b5f
matthewchiccino <matthew.chiccino@outlook.com>
2025-07-13 21:50:39 -0400

    real time slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 import Upload from './sections/Upload.jsx'
@@ -7,6 +7,16 @@ import Settings from './sections/Settings.jsx'
 
 function App() {
   const [imageData, setImageData] = useState(null) // ← this will hold the image
+  const [charAmount, setCharAmount] = useState(100)
+  const [brightness, setBrightness] = useState(1)
+
+  useEffect(() => {
+    console.log('Character Amount changed:', charAmount)
+  }, [charAmount])
+
+  useEffect(() => {
+    console.log('Brightness changed:', brightness)
+  }, [brightness])
 
   return (
     <>
@@ -15,10 +25,15 @@ function App() {
           <Upload onUpload={setImageData}/> {/* Pass the setter function as a prop */}
         </div>
         <div className="display-section">   {/* Pass imageData to Display */}
-          <Display imageData={imageData} />
+          <Display imageData={imageData} charAmount={charAmount} brightness={brightness}/>
         </div>
         <div className="settings-section">
-          <Settings />
+          <Settings
+            charAmount={charAmount}
+            setCharAmount={setCharAmount}
+            brightness={brightness}
+            setBrightness={setBrightness}
+          />
         </div>
       </div>
     </>
